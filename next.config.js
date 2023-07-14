e4796574b9e34a3bf9ad6354435ec5a773e8247c
ROSDAD <dadlani.r@northeastern.edu>
2023-07-14 03:36:31 -0400

    next.config updated again

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextImages = require("next-images");
 
 module.exports = withPlugins([nextImages], {
   images: {
-    disableStaticImages: true,
+    disableStaticImages: false,
   },
 });
 module.exports ={
